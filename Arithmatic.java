55634ea11afd989d817192b4384476af74fdbece
shivamkumbhar26 <shivamkumbhar2612@gmail.com>
2025-12-10 20:11:24 +0530

    Second commit & .gitignore added

---
+++ Arithmatic.java
@@ -0,0 +1,10 @@
+public class Arithmatic {
+    
+    public static void addNumbers( int a , int b){
+        System.out.println(" a + b : " +(a+b) );
+    }
+    public static void subtractNumbers( int a , int b){
+        System.out.println(" a - b : " + (a-b) );
+    }
+
+}
--- Hello.java
+++ Hello.java
@@ -1,8 +1,16 @@
-
+import java.util.Scanner ;
+ 
 class Hello {
 
     public static void main(String [] args){
         System.out.println("hello world ");
+        
+        Scanner sc = new Scanner(System.in) ; 
+        System.out.println("enter number 1 : ");
+        int a = sc.nextInt();
+        System.out.println("enter number 2 : ");
+        int b = sc.nextInt();
+        Arithmatic.addNumbers(a , b);
     }
 
 }
