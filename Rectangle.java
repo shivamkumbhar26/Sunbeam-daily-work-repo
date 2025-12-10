fecb0c4f6e3da30547e3acd6b5b173dd281acb29
shivamkumbhar26 <shivamkumbhar2612@gmail.com>
2025-12-10 21:12:45 +0530

    created file Rectangle.java from another(other working directory) repo to test git pull from main day 01 directory

---
+++ Rectangle.java
@@ -0,0 +1,10 @@
+class Rectangle{
+
+    public static void area(int ln , int wd ){
+        System.out.println("Area : " + (ln*wd));
+    }
+    public static void perimeter(int ln , int wd ){
+        System.out.println("Area : " + (2*(ln+wd)));
+    }
+    
+}
